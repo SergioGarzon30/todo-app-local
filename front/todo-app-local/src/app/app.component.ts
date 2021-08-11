b253be958d07d567074a9fe4a0d8f0e24c36fce3
Sergio Garzon <sergiojat22@gmail.com>
2021-08-10 20:18:29 -0500

    Clase Interpoblacion y Directivas

--- front/todo-app-local/src/app/app.component.ts
+++ front/todo-app-local/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app-local';
+
+  nombre: string = "Sergio";
+  nombreGuardado: string = "";
+  edad: number = 18;
+  casado: boolean = false;
+  listaNombres: Array<string>=["Alejandro", "Cristian", "Sergio","Rogger"];
+
+
+  guardar(){
+    this.nombreGuardado = this.nombre;
+    this.listaNombres.push(this.nombre);
+  
+  
+  }
 }
